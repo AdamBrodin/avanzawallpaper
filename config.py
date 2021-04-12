4a3ffcc0cfb507cf42e4d8232eb8a3c2fef91746
Adam Brodin <worecraft@gmail.com>
2021-04-13 00:27:26 +0200

    Fixed config

--- config.py
+++
@@ -1,9 +0,0 @@
-# CONSTANTS
-avanzaUsername = "adambrodin"
-avanzaPassword = ""
-totpSecret = ""
-accountID = ""
-updateIntervalSeconds = (
-    120  # How often the wallpaper refreshes (too low values will cause API issues)
-)
-backgroundColor = (0, 0, 255)
