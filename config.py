f2de1add3ca7f30dae2a40b9744f6dce9158a17e
Adam Brodin <worecraft@gmail.com>
2021-04-13 03:17:16 +0200

    Removed ignored files

--- config.py
+++
@@ -1,8 +0,0 @@
-# CONSTANTS
-avanzaUsername = "AVANZA_USERNAME"
-avanzaPassword = "AVANZA_PASSWORD"
-totpSecret = "TOPT_SECRET"
-accountID = "ACCOUNT_ID"
-# How often the wallpaper refreshes (too low values will cause API issues)
-updateIntervalSeconds = 120
-backgroundColor = (0, 0, 255)
