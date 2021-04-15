4fd73a00584935b807247d8a33123e7b2bdf3917
Adam Brodin <worecraft@gmail.com>
2021-04-15 23:34:52 +0200

    Updated config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# CONSTANTS
+avanzaUsername = "AVANZA_USERNAME"
+avanzaPassword = "AVANZA_PASSWORD"
+totpSecret = "AVANZA_2FACTOR_TOPT_SECRET"
+accountID = "AVANZA_ACCOUNT_ID"
+
+# How often the wallpaper refreshes (too low values will cause API issues)
+updateIntervalSeconds = 120
+backgroundColor = (3, 169, 244)
