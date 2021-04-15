7add3da14fdaf593b2f7ebe85f8e19c6cb6eccee
Adam Brodin <worecraft@gmail.com>
2021-04-15 23:31:58 +0200

    Added time & day checks to reduce API load outside market hours

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import ctypes
-import time
+import time as sleeptime
+from datetime import datetime, time
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
@@ -16,38 +17,48 @@ avanza = Avanza(
     }
 )
 
+
+def in_market_hours(market_open, market_close):
+    current_time = datetime.now().time()
+    return current_time >= market_open and current_time <= market_close
+
+
 while True:
     try:
-        overview = avanza.get_account_overview(config.accountID)
-        ownedCapital = str(int(overview["ownCapital"]))
-        balanceText = ownedCapital + " sek"
+        # Only execute API calls on weekdays (to minimize load on API)
+        if datetime.today().weekday() < 5 and in_market_hours(
+            time(9, 00), time(22, 00)
+        ):
+            overview = avanza.get_account_overview(config.accountID)
+            ownedCapital = str(int(overview["ownCapital"]))
+            balanceText = ownedCapital + " sek"
 
-        # Generate image
-        W, H = (1920, 1080)
-        img = Image.new("RGBA", (W, H), config.backgroundColor)
-        draw = ImageDraw.Draw(img)
-        myFont = ImageFont.truetype("font.ttf", size=75)
-        w, h = draw.textsize(balanceText, font=myFont)
+            # Generate image
+            W, H = (1920, 1080)
+            img = Image.new("RGBA", (W, H), config.backgroundColor)
+            draw = ImageDraw.Draw(img)
+            myFont = ImageFont.truetype("font.ttf", size=75)
+            w, h = draw.textsize(balanceText, font=myFont)
 
-        # Draws centered text
-        draw.text(
-            ((W - w) / 2, (H - h) / 2),
-            balanceText,
-            font=myFont,
-            fill="white",
-            align="center",
-        )
-        img.save("balance.png", "PNG")
+            # Draws centered text
+            draw.text(
+                ((W - w) / 2, (H - h) / 2),
+                balanceText,
+                font=myFont,
+                fill="white",
+                align="center",
+            )
+            img.save("balance.png", "PNG")
 
-        # Set as wallpaper
-        ctypes.windll.user32.SystemParametersInfoW(
-            20,
-            0,
-            os.getcwd() + "\\balance.png",
-            0,
-        )
+            # Set as wallpaper
+            ctypes.windll.user32.SystemParametersInfoW(
+                20,
+                0,
+                os.getcwd() + "\\balance.png",
+                0,
+            )
 
         # Sleeps for x seconds before looping again
-        time.sleep(config.updateIntervalSeconds)
+        sleeptime.sleep(config.updateIntervalSeconds)
     except Exception as e:
         print("Error occurred: ", e)
